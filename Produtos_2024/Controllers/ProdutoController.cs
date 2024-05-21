f3926eda0883e72dda0ebbfb36492d1ae53a4a27
Leandro Santos <leandro22.js@gmail.com>
2024-05-20 23:48:02 -0300

    Criando a index vinculada a Produto e trocando a ancora em _Layout

---
+++ Produtos_2024/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Produtos_2024.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
